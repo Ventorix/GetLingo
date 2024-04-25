7d9d2254cbadbef9b78840767aa6fcb7f88c5dfb
1Fenix7 <userpk66@gmail.com>
2024-04-25 05:35:00 +0300

    feat(database): :card_file_box: add query to get units info from database

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { auth } from '@clerk/nextjs/server';
 import { eq } from 'drizzle-orm';
 import { cache } from 'react';
 import db from './drizzle';
-import { courses, userProgress } from './schema';
+import { challengeProgress, courses, units, userProgress } from './schema';
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -19,6 +19,57 @@ export const getUserProgress = cache(async () => {
   });
 
   return data;
+});
+
+export const getUnits = cache(async () => {
+  const userProgress = await getUserProgress();
+  const { userId } = await auth();
+
+  if (!userId || !userProgress?.activeCourseId) {
+    return [];
+  }
+
+  const data = await db.query.units.findMany({
+    orderBy: (units, { asc }) => [asc(units.order)],
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        orderBy: (lessons, { asc }) => [asc(lessons.order)],
+        with: {
+          challenges: {
+            orderBy: (challenges, { asc }) => [asc(challenges.order)],
+            with: {
+              challengeProgress: {
+                where: eq(challengeProgress.userId, userId),
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const normalizedData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      if (lesson.challenges.length === 0) {
+        return { ...lesson, completed: false };
+      }
+
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return (
+          challenge.challengeProgress &&
+          challenge.challengeProgress.length > 0 &&
+          challenge.challengeProgress.every((progress) => progress.completed)
+        );
+      });
+
+      return { ...lesson, completed: allCompletedChallenges };
+    });
+
+    return { ...unit, lessons: lessonsWithCompletedStatus };
+  });
+
+  return normalizedData;
 });
 
 export const getCourses = cache(async () => {
