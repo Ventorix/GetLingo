3dfb96296b86775dcf871d640341ceea545fd4ed
1Fenix7 <userpk66@gmail.com>
2024-04-25 04:55:35 +0300

    feat(database): :hammer: update seed script to match with updated database tables

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,6 +14,11 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       {
@@ -40,6 +45,68 @@ const main = async () => {
         id: 5,
         title: 'Japanese',
         imageSrc: '/flags/JP - Japan.svg',
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: 'Unit 1',
+        description: 'Learn the basics of English',
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: 'Nouns',
+      },
+      {
+        id: 2,
+        unitId: 1,
+        order: 2,
+        title: 'Verbs',
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: 'SELECT',
+        order: 1,
+        question: 'Which one of this is the "man"?',
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: '/man.svg',
+        audioSrc: '',
+        correct: true,
+        text: 'man',
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: '/woman.svg',
+        audioSrc: '',
+        correct: false,
+        text: 'woman',
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: '/robot.svg',
+        audioSrc: '',
+        correct: true,
+        text: 'robot',
       },
     ]);
 
