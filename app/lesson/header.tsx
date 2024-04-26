15b25a56245b10c4e0a3c9843f3a07d2085f5ec8
1Fenix7 <userpk66@gmail.com>
2024-04-26 17:28:52 +0300

    feat: :sparkles: add exit modal for lesson

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/button';
 import { Progress } from '@/components/ui/progress';
+import { useExitModal } from '@/store/use-exit-modal';
 import { InfinityIcon, X } from 'lucide-react';
 import Image from 'next/image';
 
@@ -9,9 +11,13 @@ type Props = {
 };
 
 const Header = ({ hearts, percentage, hasActiveSubscription }: Props) => {
+  const { open } = useExitModal();
+
   return (
     <header className='mx-auto flex w-full max-w-[1140px] items-center justify-between gap-x-7 px-10 pt-[20px] lg:pt-[50px]'>
-      <X onClick={() => {}} className='cursor-pointer text-slate-500 transition hover:opacity-75' />
+      <Button onClick={open}>
+        <X className='h-6 w-6 text-slate-500' />
+      </Button>
       <Progress value={percentage} />
       <div className='flex items-center font-bold text-rose-500'>
         <Image src={'/heart.svg'} alt='Hearts' height={28} width={28} className='mr-2' />
