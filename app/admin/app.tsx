64b3d9ee5d8d2bb88d46cc791f1613e3d2fb2fe2
1Fenix7 <userpk66@gmail.com>
2024-05-12 23:04:19 +0300

    feat: :sparkles: add CRUD actions for units on admin page

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -5,6 +5,9 @@ import { Admin, Resource } from 'react-admin';
 import CourseCreate from './course/create';
 import CourseEdit from './course/edit';
 import CourseList from './course/list';
+import UnitCreate from './unit/create';
+import UnitEdit from './unit/edit';
+import UnitList from './unit/list';
 
 const dataProvider = simpleRestProvider('/api');
 
@@ -17,6 +20,13 @@ const App = () => {
         list={CourseList}
         create={CourseCreate}
         edit={CourseEdit}
+      />
+      <Resource
+        name='units'
+        recordRepresentation={'title'}
+        list={UnitList}
+        create={UnitCreate}
+        edit={UnitEdit}
       />
     </Admin>
   );
