1b4a4637d350dcd7664ff90594b9d12f07b5b91d
1Fenix7 <userpk66@gmail.com>
2024-05-12 18:22:23 +0300

    feat: :lipstick: add course page responsiveness enhancements

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -16,7 +16,7 @@ export const Card = ({ title, id, imageSrc, onClick, disabled, active }: Props) 
     <div
       onClick={() => onClick(id)}
       className={cn(
-        'flex h-full min-h-[167px] min-w-[140px] cursor-pointer flex-col items-center justify-between rounded-xl border-2 border-slate-200 bg-white p-3 pb-6 shadow-[0_5px_0_#e2e8f0] transition hover:bg-slate-50 active:translate-y-[5px] active:shadow-none lg:min-h-[210px] lg:min-w-[190px]',
+        'flex h-full cursor-pointer flex-col items-center justify-between rounded-xl border-2 border-slate-200 bg-white p-3 pb-6 shadow-[0_5px_0_#e2e8f0] transition hover:bg-slate-50 active:translate-y-[5px] active:shadow-none',
         disabled && 'pointer-events-none opacity-50'
       )}
     >
@@ -30,9 +30,9 @@ export const Card = ({ title, id, imageSrc, onClick, disabled, active }: Props) 
       <Image
         src={imageSrc}
         alt={title}
-        height={70}
-        width={93.33}
-        className='rounded-lg border border-neutral-300 object-cover drop-shadow-lg'
+        height={60}
+        width={83.33}
+        className='mt-2 rounded-lg border border-neutral-300 object-cover drop-shadow-lg xs:mt-0 xs:h-[70px] xs:w-[93.33px]'
       />
       <p className='mt-3 text-center font-bold text-neutral-700'>{title}</p>
     </div>
--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -29,7 +29,7 @@ export const List = ({ courses, activeCourseId }: Props) => {
   };
 
   return (
-    <div className='grid grid-cols-2 gap-4 pt-6 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))]'>
+    <div className='grid grid-cols-2 gap-4 pt-6 xl:grid-cols-[repeat(auto-fill,minmax(210px,1fr))]'>
       {courses.map((course) => (
         <Card
           key={course.id}
