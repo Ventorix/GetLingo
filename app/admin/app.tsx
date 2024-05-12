36335940d8f8b2cabf183a331c9481cb49575451
1Fenix7 <userpk66@gmail.com>
2024-05-12 23:22:06 +0300

    feat: :sparkles: add CRUD operations for lesson on admin page

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -5,6 +5,9 @@ import { Admin, Resource } from 'react-admin';
 import CourseCreate from './course/create';
 import CourseEdit from './course/edit';
 import CourseList from './course/list';
+import LessonCreate from './lesson/create';
+import LessonEdit from './lesson/edit';
+import LessonList from './lesson/list';
 import UnitCreate from './unit/create';
 import UnitEdit from './unit/edit';
 import UnitList from './unit/list';
@@ -27,6 +30,13 @@ const App = () => {
         list={UnitList}
         create={UnitCreate}
         edit={UnitEdit}
+      />
+      <Resource
+        name='lessons'
+        recordRepresentation={'title'}
+        list={LessonList}
+        create={LessonCreate}
+        edit={LessonEdit}
       />
     </Admin>
   );
